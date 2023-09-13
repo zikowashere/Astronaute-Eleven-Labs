cd69bd7203e47eb7eeaf53a31215a79da4b47dd5
zikowashere <zakdaouma@gmail.com>
2023-09-13 10:51:43 +0200

    feat: validation of an astronaut schema

--- Server/application/routes/astronaut.ts
+++ Server/application/routes/astronaut.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import { json } from "stream/consumers";
+import { ZodError } from "zod";
 import astronautController from "../../astronaut/controllers/astronautContoller";
 import { Astronaut } from "../../astronaut/entities/Astronaut";
 import { AstronautMongodbRepo } from "../repository/AstronautMongodbRepo";
+import validation from "../utils/validation";
 
 const routerAstronaut = express.Router();
 const astronautMongodbRepo = new AstronautMongodbRepo();
@@ -19,8 +22,14 @@ routerAstronaut.get("/", async (req: Request, res: Response) => {
 routerAstronaut.post("/", async (req: Request, res: Response) => {
   const astronaut: Astronaut = req.body;
   try {
-    const result = await astronautsController.addAstronaut(astronaut);
-    res.status(200).json(result);
+    const validationAstronaut = validation(astronaut);
+    if (validationAstronaut.success) {
+      const result = await astronautsController.addAstronaut(astronaut);
+      return res.status(200).json(result);
+    } else {
+      const errorMessage = JSON.parse(validationAstronaut.error.message);
+      return res.status(500).json(errorMessage[0].message);
+    }
   } catch (error) {
     res.status(500).json(error);
   }
@@ -29,12 +38,19 @@ routerAstronaut.post("/", async (req: Request, res: Response) => {
 routerAstronaut.put("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
   const astronautUpdated = req.body;
+
   try {
-    const astronautToUpdated = await astronautsController.updateAstronaut(
-      id,
-      astronautUpdated,
-    );
-    return res.status(200).json(astronautToUpdated);
+    const validationAstronaut = validation(astronautUpdated);
+    if (validationAstronaut.success) {
+      const astronautToUpdated = await astronautsController.updateAstronaut(
+        id,
+        astronautUpdated,
+      );
+      return res.status(200).json(astronautToUpdated);
+    } else {
+      const errorMessage = JSON.parse(validationAstronaut.error.message);
+      return res.status(500).json(errorMessage[0].message);
+    }
   } catch (error) {
     return res.status(500).json(error);
   }
